68c3bb3a4a55c3edb4658a308b1187533a088e43
Mayen Akpan <neroxma@yahoo.com>
2021-06-14 22:20:19 -0700

    Initial commit

---
+++ movie-website/gallery-layout/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+  const [images, setImages] = useState([
+    { url: "https://source.unsplash.com/random/1" },
+    { url: "https://source.unsplash.com/random/2" },
+    { url: "https://source.unsplash.com/random/3" },
+    { url: "https://source.unsplash.com/random/4" },
+    { url: "https://source.unsplash.com/random/5" },
+    { url: "https://source.unsplash.com/random/6" },
+  ]);
+
+  return (
+    <section className="hero">
+      <div className="cardContainer">
+        <Card images={images} />
+      </div>
+      <div className="btnContainer">
+        <button>Remove</button>
+        <button>Add</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
